526dfc7c48f96811cf2d028ff7d6b1fa7fa34c58
SUJIT GAWALI <sujitgavali451@gmail.com>
2026-08-18 15:49:43 +0530

    fix prod error

--- src/main/java/com/beta/hub_backend/config/SecurityConfig.java
+++ src/main/java/com/beta/hub_backend/config/SecurityConfig.java
@@ -60,7 +60,10 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://beta-hub.netlify.app"
+                )
         );
 
         configuration.setAllowedMethods(
